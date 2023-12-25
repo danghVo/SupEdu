3ec16911d5d5198cb6a9a3196335dff2a138e6bb
Vo <vob2014721@student.ctu.edu.vn>
2023-12-25 23:31:42 +0700

    add Teacher and Student image

--- src/assets/image/index.tsx
+++ src/assets/image/index.tsx
@@ -2,6 +2,8 @@ const image = {
     logo: require('./logo.png'),
     signIn: require('./SignIn.png'),
     google: require('./google.png'),
+    student: require('./student.png'),
+    teacher: require('./teacher.png'),
 };
 
 export default image;
