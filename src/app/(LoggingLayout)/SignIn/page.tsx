5d58f2ab5db9102590f04be75a582fe8aaedf04e
Vo <vob2014721@student.ctu.edu.vn>
2023-12-26 13:21:50 +0700

    add animation, link to SignUp

--- src/app/(LoggingLayout)/SignIn/page.tsx
+++ src/app/(LoggingLayout)/SignIn/page.tsx
@@ -1,20 +1,28 @@
 'use client';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 import Input from '~/components/Input';
 import Form from '~/components/Form';
 import image from '~/assets/image';
+import Link from 'next/link';
 
 export default function Page() {
     return (
         <div className="shadow-custom-3 flex overflow-hidden h-[80vh] w-[1000px] rounded-[50px] bg-[rgb( 225, 230, 220)]/[.1]">
             <Image src={image.signIn} className="w-[50%]" alt="background" />
-            <div className="grow flex items-center justify-center flex-col">
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className="grow flex items-center justify-center flex-col"
+            >
                 <div className="text-center flex justify-between  w-full px-12">
                     <div className="text-[18px] font-semibold">Đăng nhập</div>
                     <div>
                         Bạn chưa có tài khoản?{' '}
-                        <span className="text-[18px] font-semibold cursor-pointer underline">Đăng ký</span>
+                        <Link href={'./SignUp'} className="text-[18px] font-semibold cursor-pointer underline">
+                            Đăng ký
+                        </Link>
                     </div>
                 </div>
                 <Form
@@ -35,7 +43,7 @@ export default function Page() {
                     Đăng nhập với:{' '}
                     <Image width={30} className="ml-[8px] cursor-pointer" src={image.google} alt="Google Icon" />
                 </div>
-            </div>
+            </motion.div>
         </div>
     );
 }
