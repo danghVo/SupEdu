734d48a926b55d5d846b8aa43e96526b0b3c33e8
Vo <vob2014721@student.ctu.edu.vn>
2023-12-31 23:46:59 +0700

    add custom BackgroundColor: class, boxShadow: custom-4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,11 +16,13 @@ const config: Config = {
             },
             backgroundColor: {
                 main: 'rgb(var(--background-start-rgb))',
+                class: 'rgb(var(--background-start-rgb))',
             },
             boxShadow: {
                 'custom-1': 'rgba(0, 0, 0, 0.35) 0px 5px 15px;',
                 'custom-2': 'rgba(0, 0, 0, 0.24) 0px 3px 8px;',
                 'custom-3': 'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;',
+                'custom-4': 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;',
             },
         },
     },
