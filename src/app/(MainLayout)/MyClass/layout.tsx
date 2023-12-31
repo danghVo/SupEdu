c0a2d07f79a454df75d85d91681cc2d8dc8903e5
Vo <vob2014721@student.ctu.edu.vn>
2023-12-31 23:48:17 +0700

    delete Header of MainLayout

--- src/app/(MainLayout)/MyClass/layout.tsx
+++ src/app/(MainLayout)/MyClass/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import SideBar from '../components/SideBar';
-import Header from '../components/Header';
 
 export default function MyClassLayout({ children }: { children: React.ReactNode }) {
     return (
