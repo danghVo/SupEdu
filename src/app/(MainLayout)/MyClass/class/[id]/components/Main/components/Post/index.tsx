19ca1dd671653b289ff73c2902c59a3ba7959246
Vo <vob2014721@student.ctu.edu.vn>
2024-01-17 14:33:46 +0700

    create Post component for Main component

---
+++ src/app/(MainLayout)/MyClass/class/[id]/components/Main/components/Post/index.tsx
@@ -0,0 +1,9 @@
+import PostCard from '~/components/PostCard';
+
+export default function Post() {
+    return (
+        <div className="flex flex-col items-center">
+            <PostCard />
+        </div>
+    );
+}
