057527501c738a0e6598b8e27beaaecced482019
Vo <huynhvo47@gmail.com>
2024-01-19 23:51:17 +0700

    create: image index with .ts extension

---
+++ src/assets/image/index.ts
@@ -0,0 +1,9 @@
+const image = {
+    logo: require('./logo.png'),
+    signIn: require('./SignIn.png'),
+    google: require('./google.png'),
+    student: require('./student.png'),
+    teacher: require('./teacher.png'),
+};
+
+export default image;
