f36f59d706a70c93ca8ec4cd6dfb3ead7ee09ded
Vo <huynhvo47@gmail.com>
2024-01-21 23:41:51 +0700

    update: update loading UI when loading youtube video to increase UX

--- src/components/TextEditor/components/Video/index.tsx
+++ src/components/TextEditor/components/Video/index.tsx
@@ -1,24 +1,34 @@
 import { faCircleNotch, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { EditorBlock } from 'draft-js';
-import { Suspense } from 'react';
+import { Suspense, lazy, useState } from 'react';
+import { motion } from 'framer-motion';
 
 export default function Video(props: any) {
+    const [isLoad, setIsLoad] = useState(false);
+
     return (
-        <div {...props.blockProps} className="relative px-[12px] my-[12px] mr-[14px]">
+        <div onClick={props.blockProps.onClick} className="relative px-[12px] my-[12px] mr-[14px]">
             {props.blockProps.src ? (
-                <Suspense
-                    fallback={
-                        <div className="bg-slate-400 h-full">
-                            <FontAwesomeIcon icon={faCircleNotch} />
+                <>
+                    {!isLoad && (
+                        <div className="w-full h-[350px] flex items-center justify-center bg-slate-200">
+                            <motion.span
+                                className="text-[20px]"
+                                initial={{ rotate: 0 }}
+                                animate={{ rotate: 360 }}
+                                transition={{ repeat: Infinity, duration: 1 }}
+                            >
+                                <FontAwesomeIcon icon={faCircleNotch} />
+                            </motion.span>
                         </div>
-                    }
-                >
+                    )}
                     <iframe
-                        className="w-full h-[350px]"
+                        className={`${isLoad ? '' : 'hidden'} w-full h-[350px]`}
                         src={'https://www.youtube.com/embed/' + props.blockProps.src}
+                        onLoad={() => setIsLoad(true)}
                     ></iframe>
-                </Suspense>
+                </>
             ) : (
                 <>
                     <div className="bg-slate-200 px-[12px] py-[8px] rounded-l-lg">
