f20a0cf50c79359445554ce52f5dba11990fd7ce
Vo <huynhvo47@gmail.com>
2024-02-01 13:32:53 +0700

    created: FileType type, TimeData type, fileTyles array to store extension have img

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -13,6 +13,8 @@ export const month = [
     'December',
 ];
 
+export const hour = new Array(24).fill(-1).map((item, index) => index);
+
 export const day = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
 export const classDetailSections = [{ name: 'Lớp học' }, { name: 'Bài tập' }, { name: 'Thành viên' }];
@@ -22,3 +24,30 @@ export const postType = ['Thông báo', 'Bài tập', 'Vote'];
 export const buttonActionName = ['Đăng', 'Đặt hẹn', 'Lưu'];
 
 export const fontSizeEditor = ['normal', 'medium', 'big'];
+
+export interface FileType {
+    name: string;
+    path: string;
+    type: string;
+}
+
+export interface TimeData {
+    date: string;
+    time: number;
+}
+
+export const fileTypes = [
+    'csv',
+    'docx',
+    'exe',
+    'gif',
+    'html',
+    'iso',
+    'java',
+    'javascript',
+    'jpg',
+    'rar',
+    'png',
+    'rar',
+    'xml',
+];
