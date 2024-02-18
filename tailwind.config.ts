8e1b6ccf2d9a4580d87616e13b76cbd4749255f3
Vo <huynhvo47@gmail.com>
2024-02-18 10:59:02 +0700

    updated: updated animation loading skeleton

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -25,6 +25,15 @@ const config: Config = {
                 'custom-4': 'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px',
                 'custom-5': 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
             },
+            animation: {
+                loading: 'loading 5s linear infinite',
+            },
+            keyframes: {
+                loading: {
+                    '0%': { left: '0' },
+                    '100%': { left: '100%' },
+                },
+            },
         },
     },
     plugins: [],
