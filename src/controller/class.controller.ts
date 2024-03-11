5c5e6bd33b03d0e3412ec16666959aa205663cb7
Vo <huynhvo47@gmail.com>
2024-03-11 07:09:26 +0700

    updated: updated methods

--- src/controller/class.controller.ts
+++ src/controller/class.controller.ts
@@ -1,19 +1,53 @@
+import { FormData } from '~/app/(ClientLayout)/(MainLayout)/class/components/createClassModal';
 import { ClassService } from '~/service';
 
 export class ClassController {
     // Your code here
+    async getAllClass() {
+        const classService = new ClassService();
+
+        const res = await classService.getAllClass();
+
+        return res.data;
+    }
+
     async getClasses() {
         const classService = new ClassService();
 
-        return await classService.getClasses();
+        const res = await classService.getClasses();
+
+        return res.data;
     }
 
-    async getClass() {
+    async getClass(classUuid: string) {
         // Your code here
+        const classService = new ClassService();
+
+        const res = await classService.getClass(classUuid);
+
+        return res.data;
     }
 
-    async createClass() {
-        // Your code here
+    async createClass(formData: FormData, userUuid: string) {
+        const classService = new ClassService();
+
+        const theme = `from-[${formData.theme.from}] to-[${formData.theme.to}]`;
+
+        const res = await classService.createClass({
+            ...formData,
+            theme,
+            teacher: userUuid,
+        });
+
+        return res.data;
+    }
+
+    async joinClass(classUuid: string, userUuid: string) {
+        const classService = new ClassService();
+
+        const res = await classService.joinClass(classUuid, userUuid);
+
+        return res.data;
     }
 
     async updateClass() {
