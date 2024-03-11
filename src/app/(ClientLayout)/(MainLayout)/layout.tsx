6ab8954e5b38f7d97dc6331a0fa796a851d2d844
Vo <huynhvo47@gmail.com>
2024-03-11 07:08:52 +0700

    updated: fetch profile of user when have token or move back to signin

--- src/app/(ClientLayout)/(MainLayout)/layout.tsx
+++ src/app/(ClientLayout)/(MainLayout)/layout.tsx
@@ -1,6 +1,32 @@
+'use client';
+
+import { useEffect } from 'react';
 import SideBar from './components/SideBar';
+import { UserController } from '~/controller';
+import { useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
 
 export default function layout({ children }: { children: React.ReactNode }) {
+    const queryClient = useQueryClient();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (window.localStorage.getItem('token')) {
+            fetchProfile();
+        } else router.push('/SignIn');
+    }, []);
+
+    const fetchProfile = async () => {
+        const user = new UserController();
+
+        const data = await queryClient.fetchQuery({
+            queryKey: ['user'],
+            queryFn: () => user.profile(),
+        });
+
+        queryClient.setQueryData(['user'], data);
+    };
+
     return (
         <div className="min-h-screen flex">
             <SideBar />
