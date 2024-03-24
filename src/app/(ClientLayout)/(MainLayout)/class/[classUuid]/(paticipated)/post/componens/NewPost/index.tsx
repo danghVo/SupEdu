c2621956b9aa05f54baf7b600db3249e5d49de6d
Vo <huynhvo47@gmail.com>
2024-03-24 22:40:16 +0700

    updated: add handleCreatePost function to create new post

--- src/app/(ClientLayout)/(MainLayout)/class/[classUuid]/(paticipated)/post/componens/NewPost/index.tsx
+++ src/app/(ClientLayout)/(MainLayout)/class/[classUuid]/(paticipated)/post/componens/NewPost/index.tsx
@@ -1,11 +1,30 @@
 import { faCirclePlus, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useQueryClient } from '@tanstack/react-query';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useState } from 'react';
 import PostCard from '~/components/PostCard';
+import { PostController } from '~/controller';
 
-export default function NewPost() {
+export default function NewPost({ classUuid }: { classUuid: string }) {
     const [isNewPost, setIsNewPost] = useState(false);
+    const queryClient = useQueryClient();
+
+    const handleCreatePost = async (payload: any) => {
+        const postController = new PostController();
+
+        const res = await queryClient.fetchQuery({
+            queryKey: ['createPost'],
+            queryFn: () => postController.createPost(payload, classUuid),
+        });
+
+        if (res && !res.error) {
+            await queryClient.invalidateQueries({
+                queryKey: ['posts', classUuid],
+            });
+            setIsNewPost(false);
+        }
+    };
 
     return (
         <>
@@ -42,7 +61,7 @@ export default function NewPost() {
                             exit={{ height: 0, marginBottom: 0, opacity: 0 }}
                             className="w-full flex justify-center z-0"
                         >
-                            <PostCard edit />
+                            <PostCard edit handleSubmit={handleCreatePost} />
                         </motion.div>
                     )}
                 </AnimatePresence>
