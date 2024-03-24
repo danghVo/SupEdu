fe53acb34de7c3f25109a69d130baa1b00c4ccc2
Vo <huynhvo47@gmail.com>
2024-03-24 21:21:04 +0700

    modified: removed fetchClass and replace by call function in getAllClass

--- src/app/(ClientLayout)/(MainLayout)/class/[classUuid]/layout.tsx
+++ src/app/(ClientLayout)/(MainLayout)/class/[classUuid]/layout.tsx
@@ -1,16 +1,9 @@
-// import {  } from '@tanstack/react-query';
-
-import { Query, QueryClient } from '@tanstack/react-query';
 import { ClassController } from '~/controller/class.controller';
 
 export async function generateStaticParams() {
-    const queryClient = new QueryClient();
     const classController = new ClassController();
 
-    const res = await queryClient.fetchQuery({
-        queryKey: ['classes'],
-        queryFn: () => classController.getAllClass(),
-    });
+    const res = await classController.getAllClass();
 
     const data = res.map((item: { uuid: string }) => ({ classUuid: item.uuid }));
 
