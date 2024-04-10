42551fc50a4c2514689c583c316a7f5072662ad3
Vo <huynhvo47@gmail.com>
2024-04-10 21:36:33 +0700

    updated: removed online UI, added show email, removed role props

--- src/app/(ClientLayout)/(MainLayout)/class/[classUuid]/(paticipated)/components/MemberCard/index.tsx
+++ src/app/(ClientLayout)/(MainLayout)/class/[classUuid]/(paticipated)/components/MemberCard/index.tsx
@@ -1,14 +1,13 @@
-import { faComment, faUserMinus } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
+
 import image from '~/assets/image';
 import { roleTranslate } from '~/constant';
 
 export interface MemberCardInfor {
     name: string;
-    image?: string;
-    role?: string;
-    isOnline?: boolean;
+    email: string;
+    avatar: string | null;
+    role: string;
 }
 
 export default function MemberCard({
@@ -25,18 +24,18 @@ export default function MemberCard({
             className={`flex items-center justify-between w-[80%] h-[60px] bg-white px-[12px] py-[8px] shadow-custom-4 rounded-xl ${className}`}
         >
             <div className="flex items-center">
-                {typeof infor.isOnline !== 'undefined' && (
-                    <div
-                        className={`w-[14px] h-[14px] rounded-full mx-[12px] ${infor.isOnline ? 'bg-green-700' : 'bg-slate-400'}`}
-                    ></div>
-                )}
                 <Image
-                    src={infor.image || image.student}
+                    src={
+                        infor.avatar !== null ? infor.avatar : infor.role === 'TEACHER' ? image.teacher : image.student
+                    }
+                    width={32}
+                    height={32}
                     alt={infor.name}
-                    className="w-[32px] h-[32px] mr-[16px] rounded-full"
+                    className="mr-[16px] rounded-full"
                 />
                 <div className="flex items-end gap-[8px]">
                     <div className="text-[18px] font-semibold">{infor.name}</div>
+                    <div className="text-[16px] ">{infor.email}</div>
                     {infor.role && <div className="text-[14px] text-slate-500">{roleTranslate[`${infor.role}`]}</div>}
                 </div>
             </div>
