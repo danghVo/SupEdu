5a5c35171bd31873b09ea927297026d0ac5895e3
Vo <huynhvo47@gmail.com>
2024-04-10 21:50:42 +0700

    updated: updated props, removed handleSubmit,

--- src/app/(ClientLayout)/(MainLayout)/class/[classUuid]/(paticipated)/exercise/[exerciseId]/score/components/MemberScoreCard/components/MarkScoreModal/index.tsx
+++ src/app/(ClientLayout)/(MainLayout)/class/[classUuid]/(paticipated)/exercise/[exerciseId]/score/components/MemberScoreCard/components/MarkScoreModal/index.tsx
@@ -1,15 +1,26 @@
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { RawDraftContentState } from 'draft-js';
 import { useState } from 'react';
+
 import Button from '~/components/Button';
 import Input from '~/components/Input';
 import Modal from '~/components/Modal';
 import TextEditor from '~/components/TextEditor';
 
-export default function MarkScoreModal({ handleCloseModal }: { handleCloseModal: () => void }) {
-    const [score, setScore] = useState('0');
-    const [comment, setComment] = useState<RawDraftContentState | null>(null);
+export default function MarkScoreModal({
+    initScore = '0',
+    initFeedback = null,
+    handleSubmit,
+    handleCloseModal,
+}: {
+    initScore?: string;
+    initFeedback?: string | null;
+    handleSubmit: (score: string, feedback: string) => void;
+    handleCloseModal: () => void;
+}) {
+    const [score, setScore] = useState(initScore);
+    const [feedback, setFeedback] = useState<RawDraftContentState | null>(
+        initFeedback ? JSON.parse(initFeedback) : initFeedback,
+    );
 
     const handleInputScore = (value: string) => {
         if ((value.match(/^[0-9]*$/g) && value.length < 3) || value === '100') {
@@ -23,16 +34,9 @@ export default function MarkScoreModal({ handleCloseModal }: { handleCloseModal:
         }
     };
 
-    const handleSubmit = () => {
-        console.log([score, comment]);
-    };
-
     return (
-        <Modal handleCloseModal={handleCloseModal} width={'500px'} height={'fit-conent'}>
-            <div className="px-[12px] py-[24px] relative">
-                <div className="absolute right-[16px] top-[16px] text-[24px] cursor-pointer" onClick={handleCloseModal}>
-                    <FontAwesomeIcon icon={faXmark} />
-                </div>
+        <Modal handleCloseModal={handleCloseModal} width={'w-[400px]'} height={'h-fit'}>
+            <div className="px-[12px] py-[24px]">
                 <div className="text-center text-[24px] font-bold mb-[12px]">Chấm điểm</div>
                 <div className="flex mb-[12px] px-[12px]">
                     <div className="text-[18px] mr-[16px] font-semibold">Điểm:</div>
@@ -41,17 +45,21 @@ export default function MarkScoreModal({ handleCloseModal }: { handleCloseModal:
                         value={score}
                         onChange={handleInputScore}
                         classNameWrapper="rounded-xl w-[50px] h-[30px]"
-                        className="text-center pr-[12px]"
+                        className="text-center pl-0 pr-0"
                         reset={false}
                     />
                     <span className="ml-[2px]">/100</span>
                 </div>
                 <div className="flex">
-                    <TextEditor editable rawContentState={comment} onChange={setComment} label="Nhận xét" />
+                    <TextEditor editable rawContentState={feedback} onChange={setFeedback} label="Nhận xét" />
                 </div>
 
                 <div className="flex justify-center">
-                    <Button theme="fill" handleClick={handleSubmit} className="rounded-lg mt-[12px] w-full mx-[12px]">
+                    <Button
+                        theme="fill"
+                        handleClick={() => handleSubmit(score, JSON.stringify(feedback))}
+                        className="rounded-lg mt-[12px] w-full mx-[12px]"
+                    >
                         Xác nhận
                     </Button>
                 </div>
