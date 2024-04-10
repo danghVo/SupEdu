3d557ff571171e01f9d972ea9ec984e659a49e23
Vo <huynhvo47@gmail.com>
2024-04-10 21:17:25 +0700

    updated: add Notification UI and conext to show notification

--- src/app/(ClientLayout)/(MainLayout)/layout.tsx
+++ src/app/(ClientLayout)/(MainLayout)/layout.tsx
@@ -2,11 +2,17 @@
 
 import SideBar from './components/SideBar';
 import { useRouter } from 'next/navigation';
+import { createContext, useRef } from 'react';
+import Notification, { NotificationType } from '~/components/Notification';
 import useProfile from '~/hooks/useProfile';
+
+export const NotificationTheme = createContext<(message: string, type: NotificationType) => void>(() => {});
 
 export default function layout({ children }: { children: React.ReactNode }) {
     const { data, isSuccess } = useProfile();
     const router = useRouter();
+
+    const notificationRef = useRef<{ show: (message: string, type: NotificationType) => void } | null>(null);
 
     if (isSuccess) {
         if (data.error) {
@@ -15,9 +21,16 @@ export default function layout({ children }: { children: React.ReactNode }) {
     }
 
     return (
-        <div className="min-h-screen flex">
-            <SideBar />
-            <div className="grow bg-main ml-[48px]">{children}</div>
-        </div>
+        <NotificationTheme.Provider
+            value={(message, type) => {
+                notificationRef.current?.show(message, type);
+            }}
+        >
+            <div className="h-screen flex relative overflow-hidden">
+                <Notification ref={notificationRef} />
+                <SideBar />
+                <div className="grow bg-main ml-[48px]">{children}</div>
+            </div>
+        </NotificationTheme.Provider>
     );
 }
