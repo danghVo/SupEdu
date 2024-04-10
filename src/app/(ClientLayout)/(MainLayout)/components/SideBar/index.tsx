56e3068cb4dd7d44f56d7971efda63aca8c0c167
Vo <huynhvo47@gmail.com>
2024-04-10 22:11:44 +0700

    updated: socket to get realtime data, handleLogout to fetch log out, handleClearAll to fetch clear all notification and created clear all message button

--- src/app/(ClientLayout)/(MainLayout)/components/SideBar/index.tsx
+++ src/app/(ClientLayout)/(MainLayout)/components/SideBar/index.tsx
@@ -1,38 +1,93 @@
 'use client';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { faBell, faComment, faCalendar, faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChalkboard, faGear, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
-
-import image from '~/assets/image';
+import { faBell, faComment } from '@fortawesome/free-regular-svg-icons';
+import { faChalkboard, faRightFromBracket, faUserEdit } from '@fortawesome/free-solid-svg-icons';
+import { io } from 'socket.io-client';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
-import Announcenment from '../Annoucement';
-import useProfile from '~/hooks/useProfile';
 import { useQueryClient } from '@tanstack/react-query';
+
+import { NotificationController, UserController } from '~/controller';
+import image from '~/assets/image';
+import { useClasses, useProfile, useNotification } from '~/hooks';
+import Announcenment from '../Annoucement';
+
+import Button from '~/components/Button';
 
 const mainItem = [
     { name: 'Lớp học', to: '/class', icon: faChalkboard },
+    { name: 'Hồ sơ', to: '/profile', icon: faUserEdit },
     { name: 'Chat', to: '/chat', icon: faComment },
-    // { name: 'Thông báo', to: '#', icon: faBell },
-    { name: 'Lịch', to: '/calendar', icon: faCalendar },
 ];
 
 export default function SideBar() {
     const pathName = usePathname();
     const [open, setOpen] = useState(false);
+    const { data: notifications, isSuccess: isNotificationSuccess } = useNotification();
+    const { data: classes, isSuccess: isClassesSuccess } = useClasses();
+    const { data: user, isSuccess: isUserSuccess } = useProfile();
     const [active, setActive] = useState(mainItem.findIndex((item) => pathName.includes(item.to)));
     const [openAnnounce, setOpenAnnounce] = useState(false);
-    const { data: user, isSuccess } = useProfile();
     const router = useRouter();
     const queryClient = useQueryClient();
 
+    useEffect(() => {
+        if (isClassesSuccess && isUserSuccess && !classes.error) {
+            const socket = io('http://localhost:4000');
+
+            socket.on('connect', () => {
+                classes.forEach((classItem: any) => {
+                    socket.on(`${classItem.uuid}/notification`, () => {
+                        queryClient.invalidateQueries({
+                            queryKey: ['notification'],
+                        });
+                    });
+                });
+
+                socket.on(`${user.uuid}/notification`, () => {
+                    queryClient.invalidateQueries({
+                        queryKey: ['notification'],
+                    });
+                });
+            });
+
+            return () => {
+                if (socket) {
+                    socket.disconnect();
+                }
+            };
+        }
+    }, [isClassesSuccess, isUserSuccess]);
+
     const handleLogout = async () => {
-        window.localStorage.removeItem('token');
-        router.push('/SignIn');
+        const userController = new UserController();
+        const data = await userController.logOut(user.uuid);
+        if (!data.error) {
+            window.localStorage.removeItem('token');
+            queryClient.cancelQueries();
+            router.push('/SignIn');
+        }
+    };
+
+    const countUnread =
+        (!notifications?.error &&
+            notifications?.filter((notification: { isRead: boolean }) => !notification.isRead).length) ||
+        0;
+
+    const handleClearAll = async () => {
+        const notificationController = new NotificationController();
+
+        const res = await notificationController.deleteAllNotification();
+
+        if (!res.error) {
+            queryClient.invalidateQueries({
+                queryKey: ['notification'],
+            });
+        }
     };
 
     return (
@@ -57,16 +112,40 @@ export default function SideBar() {
                     </div>
 
                     <div className="my-[16px] border-b-2 border-solid border-slate-100">
-                        <div
-                            onClick={() => setOpenAnnounce((prev) => !prev)}
-                            className={`flex items-center  ${!open ? 'justify-center' : 'px-[16px] mx-[8px]'}
-                    text-[16px] py-[12px] my-[24px] cursor-pointer relative ${openAnnounce ? 'bg-slate-500 rounded-full text-white' : ''}`}
-                        >
-                            <div className="relative z-10 whitespace-nowrap">
-                                <FontAwesomeIcon icon={faBell} className="text-[24px]" />
-                                {open && <span className="ml-[8px] font-medium">Thông báo</span>}
+                        {isNotificationSuccess && (
+                            <div className="my-[24px]">
+                                <div
+                                    onClick={() => setOpenAnnounce((prev) => !prev)}
+                                    className={`flex items-center  ${!open ? 'justify-center' : 'px-[16px] mx-[8px]'}
+                        text-[16px] py-[12px] cursor-pointer relative ${openAnnounce ? 'bg-slate-500 rounded-full text-white' : ''}`}
+                                >
+                                    <div className="relative z-10 whitespace-nowrap flex">
+                                        <div className="relative">
+                                            <FontAwesomeIcon icon={faBell} className="text-[24px]" />
+                                            {countUnread > 0 && (
+                                                <div className="absolute px-[6px] text-[14px] bg-red-500 rounded-full text-white top-[-12px] right-[-12px]">
+                                                    {countUnread}
+                                                </div>
+                                            )}
+                                        </div>
+                                        {open && <span className="ml-[12px] font-medium">Thông báo</span>}
+                                    </div>
+                                </div>
+                                {openAnnounce && (
+                                    <Button
+                                        disabled={notifications.length === 0}
+                                        className="w-[240px] mt-[12px] px-[32px]"
+                                        theme="fill"
+                                        handleClick={() => {
+                                            handleClearAll();
+                                        }}
+                                    >
+                                        Xóa tất cả thông báo
+                                    </Button>
+                                )}
                             </div>
-                        </div>
+                        )}
+
                         {mainItem.map((item, index) => (
                             <Link
                                 onClick={() => setActive(index)}
@@ -88,7 +167,7 @@ export default function SideBar() {
                                 </AnimatePresence>
                                 <div className="relative z-10 whitespace-nowrap">
                                     <FontAwesomeIcon icon={item.icon} className="text-[20px]" />
-                                    {open && <span className="ml-[8px] font-medi    um">{item.name}</span>}
+                                    {open && <span className="ml-[8px] font-medium">{item.name}</span>}
                                 </div>
                             </Link>
                         ))}
@@ -99,15 +178,7 @@ export default function SideBar() {
                     <div className=" border-b-2 border-solid border-slate-100 mb-[16px] my-[32px]">
                         <div
                             className={`whitespace-nowrap flex ${
-                                !open ? 'justify-start' : 'px-[16px] mx-[6px]'
-                            }  mb-[8px] text-[16px] w-full py-[12px] cursor-pointer`}
-                        >
-                            <FontAwesomeIcon icon={faGear} className="text-[20px]" />
-                            {open && <span className="ml-[8px] font-medium">Cài đặt</span>}
-                        </div>
-                        <div
-                            className={`whitespace-nowrap flex ${
-                                !open ? 'justify-start' : 'px-[16px] mx-[6px]'
+                                !open ? 'justify-center' : 'px-[16px] mx-[6px]'
                             }  mb-[8px] text-[16px] w-full py-[12px] cursor-pointer`}
                             onClick={() => handleLogout()}
                         >
@@ -115,21 +186,34 @@ export default function SideBar() {
                             {open && <span className="ml-[8px] font-medium">Đăng xuất</span>}
                         </div>
                     </div>
-                    {/* <div className="mb-[32px] flex items-center justify-center">
-                        <div className="w-[42px] h-[42px] flex items-center justify-center border-2 border-solid border-slate-400 rounded-full p-1 ">
-                            <Image src={image.student} alt="logo" className="w-[28px] h-[28px]" />
-                        </div> */}
-                    {/* {open && (
-                            <div className="ml-[12px] truncate max-w-full">
-                                <div className="font-bold"> {user.name} </div>
-                                <div> {user.email} </div>
+                    {isUserSuccess && (
+                        <div className="mb-[32px] flex items-center justify-center">
+                            <div className="w-[42px] h-[42px] flex items-center justify-center border-2 border-solid border-slate-400 rounded-full p-1 overflow-hidden">
+                                <Image
+                                    src={
+                                        user.avatar !== null
+                                            ? user.avatar
+                                            : user.role === 'TEACHER'
+                                              ? image.teacher
+                                              : image.student
+                                    }
+                                    width={28}
+                                    height={28}
+                                    alt="logo"
+                                />
                             </div>
-                        )} */}
-                    {/* </div> */}
+                            {open && (
+                                <div className="ml-[12px] w-[150px]">
+                                    <div className="font-bold"> {user.name} </div>
+                                    <div className="w-full truncate"> {user.email} </div>
+                                </div>
+                            )}
+                        </div>
+                    )}
                 </div>
             </motion.div>
             <AnimatePresence>
-                {openAnnounce && (
+                {openAnnounce && isNotificationSuccess && (
                     <motion.div
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
@@ -143,7 +227,7 @@ export default function SideBar() {
                             setOpen(true);
                         }}
                     >
-                        <Announcenment />
+                        <Announcenment notifications={notifications} />
                     </motion.div>
                 )}
             </AnimatePresence>
