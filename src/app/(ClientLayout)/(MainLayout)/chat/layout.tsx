4b45ea01041cf5a4b7635ff683c999b57c8b96fe
Vo <huynhvo47@gmail.com>
2024-04-10 21:17:56 +0700

    removed: removed get current chat at Layout

--- src/app/(ClientLayout)/(MainLayout)/chat/layout.tsx
+++ src/app/(ClientLayout)/(MainLayout)/chat/layout.tsx
@@ -2,20 +2,12 @@
 
 import { useEffect, useState } from 'react';
 import ChatInfor from './components/ChatInfor';
-import { usePathname } from 'next/navigation';
 
-export default function Layout({ children }: { children?: React.ReactNode }) {
-    const [currentChat, setCurrentChat] = useState<string>('');
-    const pathName = usePathname();
-
-    useEffect(() => {
-        setCurrentChat(pathName.split('/')[2]);
-    }, [pathName]);
-
+export default function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex h-screen overflow-hidden">
+        <div className="flex h-full overflow-hidden">
             <div className="w-[300px]">
-                <ChatInfor currnetChat={currentChat} />
+                <ChatInfor />
             </div>
 
             {children}
