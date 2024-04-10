44d8e3c27eeae5f8a6abb93eac547eac2e1170b8
Vo <huynhvo47@gmail.com>
2024-04-10 21:42:19 +0700

    updated: moved nav to ExerciseNav component, fetch params in generateStaticParams

--- src/app/(ClientLayout)/(MainLayout)/class/[classUuid]/(paticipated)/exercise/[exerciseId]/layout.tsx
+++ src/app/(ClientLayout)/(MainLayout)/class/[classUuid]/(paticipated)/exercise/[exerciseId]/layout.tsx
@@ -8,56 +8,28 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Link from 'next/link';
-import { redirect } from 'next/navigation';
+import { PostController } from '~/controller';
+import ExerciseNav from './components/ExerciseNav';
 
-export async function generateStaticParams({ params: { classId } }: { params: { classId: string } }) {
+export async function generateStaticParams({ params: { classUuid } }: { params: { classUuid: string } }) {
     // fetch exercise
+    const postController = new PostController();
 
-    return [
-        {
-            exerciseId: '0',
-        },
-        {
-            exerciseId: '1',
-        },
-    ];
+    const data = await postController.getAllExercises(classUuid);
+
+    return data.map((exercise: any) => ({ exerciseUuid: exercise.uuid }));
 }
 
 export default function Layout({
     children,
-    params: { classId, exerciseId },
+    params: { classUuid, exerciseId },
 }: {
     children: React.ReactNode;
-    params: { classId: string; exerciseId: string };
+    params: { classUuid: string; exerciseId: string };
 }) {
     return (
         <div>
-            <div className="flex items-center gap-[16px] mb-[32px] mt-[-16px]">
-                <Link
-                    href={`/class/${classId}/exercise`}
-                    className="w-[40px] h-[40px] bg-white shadow-custom-1 flex items-center justify-center rounded-full cursor-pointer"
-                >
-                    <FontAwesomeIcon icon={faArrowLeft} />
-                </Link>
-                <Link
-                    href={`/class/${classId}/exercise/${exerciseId}`}
-                    className="w-[40px] h-[40px] bg-white shadow-custom-1 flex items-center justify-center rounded-full cursor-pointer"
-                >
-                    <FontAwesomeIcon icon={faInbox} />
-                </Link>
-                <Link
-                    href={`/class/${classId}/exercise/${exerciseId}/score`}
-                    className="w-[40px] h-[40px] bg-white shadow-custom-1 flex items-center justify-center rounded-full cursor-pointer"
-                >
-                    <FontAwesomeIcon icon={faPenToSquare} />
-                </Link>
-                <Link
-                    href={`/class/${classId}/exercise/${exerciseId}/statistic`}
-                    className="w-[40px] h-[40px] bg-white shadow-custom-1 flex items-center justify-center rounded-full cursor-pointer"
-                >
-                    <FontAwesomeIcon icon={faChartColumn} />
-                </Link>
-            </div>
+            <ExerciseNav classUuid={classUuid} exerciseId={exerciseId} />
             {children}
         </div>
     );
