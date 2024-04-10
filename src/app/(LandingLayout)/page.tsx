648dbfa9e6f876c5e576d24b2babb78b8f60d1fe
Vo <huynhvo47@gmail.com>
2024-04-10 22:17:53 +0700

    modified: modified UI landing page

--- src/app/(LandingLayout)/page.tsx
+++ src/app/(LandingLayout)/page.tsx
@@ -1,23 +1,13 @@
-'use client';
-
-import SimpleBarReact from 'simplebar-react';
-
 export default function Landing() {
     return (
-        <SimpleBarReact style={{ maxHeight: '100vh' }} classNames={{ track: 'simplebar-track mr-2' }} forceVisible="y">
-            <div id="section-1" className="h-[100vh] max-h-[700px] relative flex flex-col items-center justify-center">
-                <div className="absolute  top-[0px] right-[50px] w-[500px] h-[500px] background-circle-2 rounded-full blur-2xl"></div>
-                <div className="absolute  top-[150px] left-[50px] w-[500px] h-[500px] background-circle-1 rounded-full blur-2xl"></div>
-                <div className="z-10">
-                    <div className="relative shadow-custom-1 rounded-full py-8 px-16">
-                        <h1 className="text-center text-8xl font-black">Welcome To SupEdu</h1>
-                    </div>
-                    <h1 className="text-center mt-10 text-[28px] cursor-pointer font-medium">Bắt Đầu</h1>
+        <div id="section-1" className="h-[100vh] max-h-[700px] relative flex flex-col items-center justify-center">
+            <div className="absolute  top-[0px] right-[50px] w-[500px] h-[500px] background-circle-2 rounded-full blur-2xl"></div>
+            <div className="absolute  top-[150px] left-[50px] w-[500px] h-[500px] background-circle-1 rounded-full blur-2xl"></div>
+            <div className="z-10">
+                <div className="relative shadow-custom-1 rounded-full py-8 px-16">
+                    <h1 className="text-center text-8xl font-black">Welcome To SupEdu</h1>
                 </div>
             </div>
-            <div id="section-2" className="h-[100vh] max-h-[700px]"></div>
-            <div id="section-3" className="h-[100vh] max-h-[700px]"></div>
-            <div id="section-4" className="h-[100vh] max-h-[700px]"></div>
-        </SimpleBarReact>
+        </div>
     );
 }
