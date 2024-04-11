7b697cd2c2d512e3ee798e56a57843b75c5c0c3a
Vo <huynhvo47@gmail.com>
2024-04-11 12:11:26 +0700

    updated: created getUui, updateProfile and changePassword method

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -5,7 +5,19 @@ export class UserService extends HTTP {
         super(window.localStorage.getItem('token') || '');
     }
 
+    async getUuid(email: string) {
+        return await this.get(`/user/uuid?email=${email}`);
+    }
+
     async profile() {
         return await this.get('/user/profile');
+    }
+
+    async updateProfile(payload: any) {
+        return await this.patch('/user/updateProfile', payload, {}, true);
+    }
+
+    async changePassword(oldPassword: string, newPassword: string, confirmPassword: string) {
+        return await this.put('/user/changePassword', { oldPassword, newPassword, confirmPassword });
     }
 }
