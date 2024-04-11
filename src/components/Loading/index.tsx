37514cbf0bb06d099eb57ab594fc8ec06b86ac5d
Vo <huynhvo47@gmail.com>
2024-04-11 11:54:33 +0700

    created: created Loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,17 @@
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { motion } from 'framer-motion';
+
+export default function Loading({ className }: { className?: string }) {
+    return (
+        <div className={`text-[64px] grow h-full flex justify-center items-center ${className}`}>
+            <motion.div
+                initial={{ rotate: 0 }}
+                animate={{ rotate: 360 }}
+                transition={{ type: 'tween', duration: 1, repeat: Infinity }}
+            >
+                <FontAwesomeIcon icon={faCircleNotch} className="" />
+            </motion.div>
+        </div>
+    );
+}
