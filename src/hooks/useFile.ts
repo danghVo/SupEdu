2338f04f47436dd6b730f577848838a468ca85e6
Vo <huynhvo47@gmail.com>
2024-04-11 12:09:16 +0700

    completed: useFile hook

--- src/hooks/useFile.ts
+++ src/hooks/useFile.ts
@@ -1,18 +1,24 @@
 import { useState } from 'react';
 import { FileType, fileExtensions } from '~/constant';
 
-export default function useFile(
-    initFiles?: Array<FileType>,
-): [Array<FileType>, (file: File) => void, (removedIndex: number) => void] {
-    const [files, setFiles] = useState<Array<FileType>>(initFiles || []);
+export default function useFile(): {
+    files: Array<FileType>;
+    fileBuffers: Array<File>;
+    setAddFile: (file: File) => void;
+    setRemoveFile: (removedIndex: number, isInBuffer: boolean) => void;
+    setInitFiles: (files: Array<any>) => void;
+} {
+    const [files, setFiles] = useState<Array<FileType>>([]);
+    const [fileBuffers, setFileBuffers] = useState<Array<File>>([]);
 
     const setAddFile = (file: File) => {
+        const url = URL.createObjectURL(file);
         const extension = file.type.split('/')[1];
         const type = fileExtensions.find((item) => item.extension === extension);
 
         const newFile = {
             name: file.name,
-            path: file.webkitRelativePath,
+            path: url,
             extension: type ? type.extension : 'unknown',
             color: type ? type.color : 'black',
         };
@@ -20,13 +26,31 @@ export default function useFile(
         setFiles((prev) => {
             return [...prev, newFile];
         });
+
+        setFileBuffers((prev) => [...prev, file]);
     };
 
-    const setRemoveFile = (removedIndex: number) => {
+    const setRemoveFile = (removedIndex: number, isInBuffer: boolean) => {
+        if (isInBuffer) {
+            setFileBuffers((prev) => prev.filter((item, index) => index !== removedIndex));
+        }
         const newFiles = files.filter((item, index) => index !== removedIndex);
 
         setFiles(newFiles);
     };
 
-    return [files, setAddFile, setRemoveFile];
+    const setInitFiles = (files: Array<FileType>) => {
+        setFiles(
+            files.map((file) => ({
+                uuid: file.uuid,
+                name: file.name,
+                extension:
+                    fileExtensions.find((extension) => extension.extension === file.extension)?.extension || 'unknown',
+                color: fileExtensions.find((extension) => extension.extension === file.extension)?.color || 'black',
+                path: file.path,
+            })),
+        );
+    };
+
+    return { files, fileBuffers, setAddFile, setRemoveFile, setInitFiles };
 }
