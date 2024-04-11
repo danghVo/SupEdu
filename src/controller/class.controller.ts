3da85b5b130b2aeee5fc1139f330838c2b283118
Vo <huynhvo47@gmail.com>
2024-04-11 12:06:30 +0700

    updated: updated controllers need for class

--- src/controller/class.controller.ts
+++ src/controller/class.controller.ts
@@ -1,60 +1,102 @@
-import { FormData } from '~/app/(ClientLayout)/(MainLayout)/class/components/createClassModal';
+import { FormData } from '~/app/(ClientLayout)/(MainLayout)/class/components/editClassModal';
 import { ClassService } from '~/service';
 
 export class ClassController {
+    classService: ClassService;
+
+    constructor() {
+        this.classService = new ClassService();
+    }
+
     // Your code here
     async getAllClass() {
-        const classService = new ClassService();
-
-        const res = await classService.getAllClass();
+        const res = await this.classService.getAllClass();
 
         return res.data;
     }
 
     async getClasses(filter?: string) {
-        const classService = new ClassService();
-
-        const res = await classService.getClasses(filter);
+        const res = await this.classService.getClasses(filter);
 
         return res.data;
     }
 
     async getClass(classUuid: string) {
         // Your code here
-        const classService = new ClassService();
 
-        const res = await classService.getClass(classUuid);
+        const res = await this.classService.getClass(classUuid);
 
         return res.data;
     }
 
-    async createClass(formData: FormData, userUuid: string) {
-        const classService = new ClassService();
+    async getCalendar(classUuid: string) {
+        const res = await this.classService.getCalendar(classUuid);
 
+        return res.data;
+    }
+
+    async createClass(formData: FormData) {
         const theme = `from-[${formData.theme.from}] to-[${formData.theme.to}]`;
 
-        const res = await classService.createClass({
+        const res = await this.classService.createClass({
             ...formData,
             theme,
-            teacher: userUuid,
         });
 
         return res.data;
     }
 
     async joinClass(classUuid: string, userUuid: string) {
-        const classService = new ClassService();
-
-        const res = await classService.joinClass(classUuid, userUuid);
+        const res = await this.classService.joinClass(classUuid, userUuid);
 
         return res.data;
     }
 
-    async updateClass() {
+    async responseJoinClass(classUuid: string, userUuid: string, approve: boolean) {
+        const res = await this.classService.responseJoinClass(classUuid, userUuid, approve);
+
+        return res.data;
+    }
+
+    async addMember(classUuid: string, email: string) {
+        const res = await this.classService.addMember(classUuid, email);
+
+        return res.data;
+    }
+
+    async removeMember(classUuid: string, userUuid: string) {
+        const res = await this.classService.removeMember(classUuid, userUuid);
+
+        return res.data;
+    }
+
+    async leaveClass(classUuid: string) {
+        const res = await this.classService.leaveClass(classUuid);
+
+        return res.data;
+    }
+
+    async updateClass(classUuid: string, formData: FormData) {
+        const theme = `from-[${formData.theme.from}] to-[${formData.theme.to}]`;
+
+        const res = await this.classService.updateClass(classUuid, {
+            ...formData,
+            theme,
+        });
+
+        return res.data;
+    }
+
+    async deleteClass(classUuid: string) {
+        const res = await this.classService.deleteClass(classUuid);
+
+        return res.data;
         // Your code here
     }
 
-    async deleteClass() {
-        // Your code here
+    async getMembers(classUuid: string) {
+        const res = await this.classService.getMembers(classUuid);
+
+        return res.data;
     }
 }
