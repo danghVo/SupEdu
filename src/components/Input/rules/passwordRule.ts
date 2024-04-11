8ce9be053c179926bed16d67796049015cfac89c
Vo <huynhvo47@gmail.com>
2024-04-11 11:54:10 +0700

    modified: modified regex of password

--- src/components/Input/rules/passwordRule.ts
+++ src/components/Input/rules/passwordRule.ts
@@ -1,6 +1,9 @@
 import validInput from './validInput';
 
 const valid = (checkedData: string) =>
-    validInput(!checkedData.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/), 'Ít nhất một kí tự số, in hoa, in thường');
+    validInput(
+        !checkedData.match(/^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[@!#$%&? ";:<>.,\/\[\]\{\}|+-_()]).*$/g),
+        'Ít nhất một kí tự số, in hoa, kí tự đặc biệt',
+    );
 
 export default { valid };
